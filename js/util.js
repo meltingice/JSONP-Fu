704690e2b5f9b4c74ea5531d2da503998c4db82d
meltingice <meltingice8917@gmail.com>
2010-10-21 23:02:27 -0400

    Initial ghPages commit

---
+++ js/util.js
@@ -0,0 +1,383 @@
+function strtotime (str, now) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Caio Ariede (http://caioariede.com)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: David
+    // +   improved by: Caio Ariede (http://caioariede.com)
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Wagner B. Soares
+    // +   bugfixed by: Artur Tchernychev
+    // %        note 1: Examples all have a fixed timestamp to prevent tests to fail because of variable time(zones)
+    // *     example 1: strtotime('+1 day', 1129633200);
+    // *     returns 1: 1129719600
+    // *     example 2: strtotime('+1 week 2 days 4 hours 2 seconds', 1129633200);
+    // *     returns 2: 1130425202
+    // *     example 3: strtotime('last month', 1129633200);
+    // *     returns 3: 1127041200
+    // *     example 4: strtotime('2009-05-04 08:30:00');
+    // *     returns 4: 1241418600
+ 
+    var i, match, s, strTmp = '', parse = '';
+
+    strTmp = str;
+    strTmp = strTmp.replace(/\s{2,}|^\s|\s$/g, ' '); // unecessary spaces
+    strTmp = strTmp.replace(/[\t\r\n]/g, ''); // unecessary chars
+
+    if (strTmp == 'now') {
+        return (new Date()).getTime()/1000; // Return seconds, not milli-seconds
+    } else if (!isNaN(parse = Date.parse(strTmp))) {
+        return (parse/1000);
+    } else if (now) {
+        now = new Date(now*1000); // Accept PHP-style seconds
+    } else {
+        now = new Date();
+    }
+
+    strTmp = strTmp.toLowerCase();
+
+    var __is =
+    {
+        day:
+        {
+            'sun': 0,
+            'mon': 1,
+            'tue': 2,
+            'wed': 3,
+            'thu': 4,
+            'fri': 5,
+            'sat': 6
+        },
+        mon:
+        {
+            'jan': 0,
+            'feb': 1,
+            'mar': 2,
+            'apr': 3,
+            'may': 4,
+            'jun': 5,
+            'jul': 6,
+            'aug': 7,
+            'sep': 8,
+            'oct': 9,
+            'nov': 10,
+            'dec': 11
+        }
+    };
+
+    var process = function (m) {
+        var ago = (m[2] && m[2] == 'ago');
+        var num = (num = m[0] == 'last' ? -1 : 1) * (ago ? -1 : 1);
+
+        switch (m[0]) {
+            case 'last':
+            case 'next':
+                switch (m[1].substring(0, 3)) {
+                    case 'yea':
+                        now.setFullYear(now.getFullYear() + num);
+                        break;
+                    case 'mon':
+                        now.setMonth(now.getMonth() + num);
+                        break;
+                    case 'wee':
+                        now.setDate(now.getDate() + (num * 7));
+                        break;
+                    case 'day':
+                        now.setDate(now.getDate() + num);
+                        break;
+                    case 'hou':
+                        now.setHours(now.getHours() + num);
+                        break;
+                    case 'min':
+                        now.setMinutes(now.getMinutes() + num);
+                        break;
+                    case 'sec':
+                        now.setSeconds(now.getSeconds() + num);
+                        break;
+                    default:
+                        var day;
+                        if (typeof (day = __is.day[m[1].substring(0, 3)]) != 'undefined') {
+                            var diff = day - now.getDay();
+                            if (diff == 0) {
+                                diff = 7 * num;
+                            } else if (diff > 0) {
+                                if (m[0] == 'last') {diff -= 7;}
+                            } else {
+                                if (m[0] == 'next') {diff += 7;}
+                            }
+                            now.setDate(now.getDate() + diff);
+                        }
+                }
+                break;
+
+            default:
+                if (/\d+/.test(m[0])) {
+                    num *= parseInt(m[0], 10);
+
+                    switch (m[1].substring(0, 3)) {
+                        case 'yea':
+                            now.setFullYear(now.getFullYear() + num);
+                            break;
+                        case 'mon':
+                            now.setMonth(now.getMonth() + num);
+                            break;
+                        case 'wee':
+                            now.setDate(now.getDate() + (num * 7));
+                            break;
+                        case 'day':
+                            now.setDate(now.getDate() + num);
+                            break;
+                        case 'hou':
+                            now.setHours(now.getHours() + num);
+                            break;
+                        case 'min':
+                            now.setMinutes(now.getMinutes() + num);
+                            break;
+                        case 'sec':
+                            now.setSeconds(now.getSeconds() + num);
+                            break;
+                    }
+                } else {
+                    return false;
+                }
+                break;
+        }
+        return true;
+    };
+
+    match = strTmp.match(/^(\d{2,4}-\d{2}-\d{2})(?:\s(\d{1,2}:\d{2}(:\d{2})?)?(?:\.(\d+))?)?$/);
+    if (match != null) {
+        if (!match[2]) {
+            match[2] = '00:00:00';
+        } else if (!match[3]) {
+            match[2] += ':00';
+        }
+
+        s = match[1].split(/-/g);
+
+        for (i in __is.mon) {
+            if (__is.mon[i] == s[1] - 1) {
+                s[1] = i;
+            }
+        }
+        s[0] = parseInt(s[0], 10);
+
+        s[0] = (s[0] >= 0 && s[0] <= 69) ? '20'+(s[0] < 10 ? '0'+s[0] : s[0]+'') : (s[0] >= 70 && s[0] <= 99) ? '19'+s[0] : s[0]+'';
+        return parseInt(this.strtotime(s[2] + ' ' + s[1] + ' ' + s[0] + ' ' + match[2])+(match[4] ? match[4]/1000 : ''), 10);
+    }
+
+    var regex = '([+-]?\\d+\\s'+
+        '(years?|months?|weeks?|days?|hours?|min|minutes?|sec|seconds?'+
+        '|sun\\.?|sunday|mon\\.?|monday|tue\\.?|tuesday|wed\\.?|wednesday'+
+        '|thu\\.?|thursday|fri\\.?|friday|sat\\.?|saturday)'+
+        '|(last|next)\\s'+
+        '(years?|months?|weeks?|days?|hours?|min|minutes?|sec|seconds?'+
+        '|sun\\.?|sunday|mon\\.?|monday|tue\\.?|tuesday|wed\\.?|wednesday'+
+        '|thu\\.?|thursday|fri\\.?|friday|sat\\.?|saturday))'+
+        '(\\sago)?';
+
+    match = strTmp.match(new RegExp(regex, 'gi')); // Brett: seems should be case insensitive per docs, so added 'i'
+    if (match == null) {
+        return false;
+    }
+
+    for (i = 0; i < match.length; i++) {
+        if (!process(match[i].split(' '))) {
+            return false;
+        }
+    }
+
+    return (now.getTime()/1000);
+}
+
+function sprintf ( ) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Ash Searle (http://hexmen.com/blog/)
+    // + namespaced by: Michael White (http://getsprink.com)
+    // +    tweaked by: Jack
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Paulo Freitas
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Brett Zamir (http://brett-zamir.me)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // *     example 1: sprintf("%01.2f", 123.1);
+    // *     returns 1: 123.10
+    // *     example 2: sprintf("[%10s]", 'monkey');
+    // *     returns 2: '[    monkey]'
+    // *     example 3: sprintf("[%'#10s]", 'monkey');
+    // *     returns 3: '[####monkey]'
+
+    var regex = /%%|%(\d+\$)?([-+\'#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuidfegEG])/g;
+    var a = arguments, i = 0, format = a[i++];
+
+    // pad()
+    var pad = function (str, len, chr, leftJustify) {
+        if (!chr) {chr = ' ';}
+        var padding = (str.length >= len) ? '' : Array(1 + len - str.length >>> 0).join(chr);
+        return leftJustify ? str + padding : padding + str;
+    };
+
+    // justify()
+    var justify = function (value, prefix, leftJustify, minWidth, zeroPad, customPadChar) {
+        var diff = minWidth - value.length;
+        if (diff > 0) {
+            if (leftJustify || !zeroPad) {
+                value = pad(value, minWidth, customPadChar, leftJustify);
+            } else {
+                value = value.slice(0, prefix.length) + pad('', diff, '0', true) + value.slice(prefix.length);
+            }
+        }
+        return value;
+    };
+
+    // formatBaseX()
+    var formatBaseX = function (value, base, prefix, leftJustify, minWidth, precision, zeroPad) {
+        // Note: casts negative numbers to positive ones
+        var number = value >>> 0;
+        prefix = prefix && number && {'2': '0b', '8': '0', '16': '0x'}[base] || '';
+        value = prefix + pad(number.toString(base), precision || 0, '0', false);
+        return justify(value, prefix, leftJustify, minWidth, zeroPad);
+    };
+
+    // formatString()
+    var formatString = function (value, leftJustify, minWidth, precision, zeroPad, customPadChar) {
+        if (precision != null) {
+            value = value.slice(0, precision);
+        }
+        return justify(value, '', leftJustify, minWidth, zeroPad, customPadChar);
+    };
+
+    // doFormat()
+    var doFormat = function (substring, valueIndex, flags, minWidth, _, precision, type) {
+        var number;
+        var prefix;
+        var method;
+        var textTransform;
+        var value;
+
+        if (substring == '%%') {return '%';}
+
+        // parse flags
+        var leftJustify = false, positivePrefix = '', zeroPad = false, prefixBaseX = false, customPadChar = ' ';
+        var flagsl = flags.length;
+        for (var j = 0; flags && j < flagsl; j++) {
+            switch (flags.charAt(j)) {
+                case ' ': positivePrefix = ' '; break;
+                case '+': positivePrefix = '+'; break;
+                case '-': leftJustify = true; break;
+                case "'": customPadChar = flags.charAt(j+1); break;
+                case '0': zeroPad = true; break;
+                case '#': prefixBaseX = true; break;
+            }
+        }
+
+        // parameters may be null, undefined, empty-string or real valued
+        // we want to ignore null, undefined and empty-string values
+        if (!minWidth) {
+            minWidth = 0;
+        } else if (minWidth == '*') {
+            minWidth = +a[i++];
+        } else if (minWidth.charAt(0) == '*') {
+            minWidth = +a[minWidth.slice(1, -1)];
+        } else {
+            minWidth = +minWidth;
+        }
+
+        // Note: undocumented perl feature:
+        if (minWidth < 0) {
+            minWidth = -minWidth;
+            leftJustify = true;
+        }
+
+        if (!isFinite(minWidth)) {
+            throw new Error('sprintf: (minimum-)width must be finite');
+        }
+
+        if (!precision) {
+            precision = 'fFeE'.indexOf(type) > -1 ? 6 : (type == 'd') ? 0 : undefined;
+        } else if (precision == '*') {
+            precision = +a[i++];
+        } else if (precision.charAt(0) == '*') {
+            precision = +a[precision.slice(1, -1)];
+        } else {
+            precision = +precision;
+        }
+
+        // grab value using valueIndex if required?
+        value = valueIndex ? a[valueIndex.slice(0, -1)] : a[i++];
+
+        switch (type) {
+            case 's': return formatString(String(value), leftJustify, minWidth, precision, zeroPad, customPadChar);
+            case 'c': return formatString(String.fromCharCode(+value), leftJustify, minWidth, precision, zeroPad);
+            case 'b': return formatBaseX(value, 2, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'o': return formatBaseX(value, 8, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'x': return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'X': return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad).toUpperCase();
+            case 'u': return formatBaseX(value, 10, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'i':
+            case 'd':
+                number = parseInt(+value, 10);
+                prefix = number < 0 ? '-' : positivePrefix;
+                value = prefix + pad(String(Math.abs(number)), precision, '0', false);
+                return justify(value, prefix, leftJustify, minWidth, zeroPad);
+            case 'e':
+            case 'E':
+            case 'f':
+            case 'F':
+            case 'g':
+            case 'G':
+                number = +value;
+                prefix = number < 0 ? '-' : positivePrefix;
+                method = ['toExponential', 'toFixed', 'toPrecision']['efg'.indexOf(type.toLowerCase())];
+                textTransform = ['toString', 'toUpperCase']['eEfFgG'.indexOf(type) % 2];
+                value = prefix + Math.abs(number)[method](precision);
+                return justify(value, prefix, leftJustify, minWidth, zeroPad)[textTransform]();
+            default: return substring;
+        }
+    };
+
+    return format.replace(regex, doFormat);
+}
+
+var date = (function () {
+	var	ONE_MIN  = 60,
+		TWO_MIN  = 120,
+	    ONE_HOUR = 3600,
+	    TWO_HOUR = 7200,
+	    ONE_DAY  = 86400,
+	    TWO_DAY  = 172800,
+	    DISPLAY_ONE_MIN  = 'less than a minute ago',
+	    DISPLAY_TWO_MIN  = 'about a minute ago',
+	    DISPLAY_ONE_HOUR = 'about %d minutes ago',
+	    DISPLAY_TWO_HOUR = 'about an hour ago',
+	    DISPLAY_ONE_DAY  = 'about %d hours ago',
+	    DISPLAY_TWO_DAY  = '1 day ago',
+	    DISPLAY_OTHER    = '%d days ago';
+	    
+	return {
+		relative_time: function (timestamp) {
+		
+			// Allows this function to take both a unix timestamp and a datetime string.
+			if ( typeof timestamp !== "number" ) {
+				timestamp = strtotime( timestamp );
+			}
+			
+			delta = Math.floor(new Date().getTime()/1000) - timestamp;
+			
+			if ( delta < ONE_MIN ) {
+				return DISPLAY_ONE_MIN;
+			} else if ( delta < TWO_MIN ) {
+				return DISPLAY_TWO_MIN;
+			} else if ( delta < ONE_HOUR ) {
+				return sprintf( DISPLAY_ONE_HOUR, Math.floor( delta / ONE_MIN ) );
+			} else if ( delta < TWO_HOUR ) {
+				return DISPLAY_TWO_HOUR;
+			} else if ( delta < ONE_DAY ) {
+				return sprintf( DISPLAY_ONE_DAY, Math.floor( delta / ONE_HOUR ) );
+			} else if ( delta < TWO_DAY ) {
+				return DISPLAY_TWO_DAY;
+			} else {
+				return sprintf( DISPLAY_OTHER, Math.floor( delta / ONE_DAY ) );
+			}
+		}
+	};
+}());
